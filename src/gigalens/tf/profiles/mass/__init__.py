875434c4310655c734031c5bd92f244dab654b24
felipe <fjurcelay@uc.cl>
2023-05-19 11:16:33 -0400

    modified __init__.py in tf mass profiles

--- src/gigalens/tf/profiles/mass/__init__.py
+++ src/gigalens/tf/profiles/mass/__init__.py
@@ -3,3 +3,5 @@ import gigalens.tf.profiles.mass.shear
 import gigalens.tf.profiles.mass.sie
 import gigalens.tf.profiles.mass.sis
 import gigalens.tf.profiles.mass.tnfw
+import gigalens.tf.profiles.mass.nfw
+import gigalens.tf.profiles.mass.piemd
