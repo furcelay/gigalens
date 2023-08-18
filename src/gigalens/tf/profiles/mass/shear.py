4ce6289099dfcc980dbfcd510b3c13699ce79b16
felipe <fjurcelay@uc.cl>
2023-08-18 12:23:52 -0400

    include hessian for Shear and EPL

--- src/gigalens/tf/profiles/mass/shear.py
+++ src/gigalens/tf/profiles/mass/shear.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
+from gigalens.tf.profile import MassProfile
 
-import gigalens.profile
 
-
-class Shear(gigalens.profile.MassProfile):
+class Shear(MassProfile):
     """External shear model, parameterized by shear components ``gamma1`` and ``gamma2``."""
 
     _name = "SHEAR"
@@ -12,3 +11,12 @@ class Shear(gigalens.profile.MassProfile):
     @tf.function
     def deriv(self, x, y, gamma1, gamma2):
         return gamma1 * x + gamma2 * y, gamma2 * x - gamma1 * y
+
+    def hessian(self, x, y, gamma1, gamma2, ra_0=0, dec_0=0):
+        gamma1 = gamma1
+        gamma2 = gamma2
+        kappa = 0.
+        f_xx = kappa + gamma1
+        f_yy = kappa - gamma1
+        f_xy = gamma2
+        return f_xx, f_xy, f_xy, f_yy
