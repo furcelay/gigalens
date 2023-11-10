dc3bb5871d27d84682a16a48ef78d98c8bc337ff
felipe <fjurcelay@uc.cl>
2023-11-10 20:40:00 -0300

    convergence and shear for all profiles

--- src/gigalens/tf/profile.py
+++ src/gigalens/tf/profile.py
@@ -6,14 +6,27 @@ class MassProfile(gigalens.profile.MassProfile):
     """Interface for a mass profile."""
 
     @tf.function
-    def hessian(self, x, y, *args, **kwargs):
+    def hessian(self, x, y, **kwargs):
         # use autograd to compute derivatives
         with tf.GradientTape(watch_accessed_variables=False, persistent=True) as tape:
             tape.watch(x)
             tape.watch(y)
-            fx, fy = self.deriv(x, y, *args, **kwargs)
+            fx, fy = self.deriv(x, y, **kwargs)
 
         f_xx, f_xy = tape.gradient(fx, [x, y])
         f_yx, f_yy = tape.gradient(fy, [x, y])
 
         return f_xx, f_xy, f_yx, f_yy
+
+    @tf.function
+    def convergence(self, x, y, **kwargs):
+        f_xx, f_xy, f_yx, f_yy = self.hessian(self, x, y, **kwargs)
+        kappa = (f_xx + f_yy) / 2
+        return kappa
+
+    @tf.function
+    def shear(self, x, y, **kwargs):
+        f_xx, f_xy, f_yx, f_yy = self.hessian(self, x, y, **kwargs)
+        gamma1 = (f_xx - f_yy) / 2
+        gamma2 = f_xy
+        return gamma1, gamma2
