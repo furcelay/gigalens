1e70da8c5c6f44fdea3b50743895035cb6872cae
furcelay <fjurcelay@uc.cl>
2024-01-02 15:33:36 -0300

    use cache memory for precomputed models: can be used for multiple positions

--- src/gigalens/tf/series/series_profile.py
+++ src/gigalens/tf/series/series_profile.py
@@ -3,6 +3,9 @@ from abc import ABC, abstractmethod
 import tensorflow as tf
 
 
+__all__ = ['MassSeries']
+
+
 class MassSeries(MassProfile, ABC):
     """
     Series expansion of the given potential on a single variable
@@ -19,18 +22,15 @@ class MassSeries(MassProfile, ABC):
         self.series_var_0 = tf.constant(0, dtype=tf.float32)
         self.constants = self._constants
         self.order = order
-        self._constants_dict = {}
-        self._f_x, self._f_y = None, None
-        self._f_xx, self._f_xy, self._f_yy = None, None, None
+        self.constants_dict = {}
+        self.deriv_cache = {}
+        self.hessian_cache = {}
         super(MassSeries, self).__init__(**kwargs)
 
-    def update_models(self, x, y, params):
+    def set_constants(self, params):
         self.series_var_0 = params[self.series_param]
-        self._constants_dict = params.copy()
-        for p in self._params:
-            del self._constants_dict[p]
-        self._f_x, self._f_y = self.precompute_deriv(x, y, **params)
-        # self._f_xx, self._f_xy, _, self._f_yy = self.precompute_hessian(x, y, **params)
+        self.constants_dict = params.copy()
+        self.clear_cache()
 
     @abstractmethod
     def precompute_deriv(self, x, y, **kwargs):
@@ -43,16 +43,18 @@ class MassSeries(MassProfile, ABC):
     def deriv(self, x, y, **kwargs):
         var = kwargs[self.series_param]
         scale = kwargs[self.scale_param]
-        f_x = self._evaluate_series(var, self._f_x)
-        f_y = self._evaluate_series(var, self._f_y)
+        f_x_series, f_y_series = self.get_cached_deriv(x, y)
+        f_x = self._evaluate_series(var, f_x_series)
+        f_y = self._evaluate_series(var, f_y_series)
         return scale * f_x, scale * f_y
 
     def hessian(self, x, y, **kwargs):
         var = kwargs[self.series_param]
         scale = kwargs[self.scale_param] / 2
-        f_xx = self._evaluate_series(var, self._f_xx)
-        f_xy = self._evaluate_series(var, self._f_xy)
-        f_yy = self._evaluate_series(var, self._f_yy)
+        f_xx_series, f_xy_series, f_yy_series = self.get_cached_hessian(x, y)
+        f_xx = self._evaluate_series(var, f_xx_series)
+        f_xy = self._evaluate_series(var, f_xy_series)
+        f_yy = self._evaluate_series(var, f_yy_series)
         return scale * f_xx, scale * f_xy, scale * f_xy, scale * f_yy
 
     @tf.function
@@ -62,5 +64,27 @@ class MassSeries(MassProfile, ABC):
         powers = tf.math.pow((tf.expand_dims(var, -1) - self.series_var_0), n)  # batch, (n+1)
         return tf.reduce_sum(coefs * powers / fact, -1)  # x, y, batch | sum along (n+1)
 
+    def get_cached_deriv(self, x, y):
+        ref = hash((x.ref(), y.ref()))
+        if ref in self.deriv_cache:
+            return self.deriv_cache[ref]
+        else:
+            value = self.precompute_deriv(x, y, **self.constants_dict)
+            self.deriv_cache[ref] = value
+            return value
 
-__all__ = [MassSeries]
+    def get_cached_hessian(self, x, y):
+        ref = hash((x.ref(), y.ref()))
+        if ref in self.hessian_cache:
+            # use precomputed value
+            # must be always the same tensors (not only the same values)
+            return self.hessian_cache[ref]
+        else:
+            # compute and save in chache
+            value = self.precompute_hessian(x, y, **self.constants_dict)
+            self.hessian_cache[ref] = value
+            return value
+
+    def clear_cache(self):
+        self.deriv_cache = {}
+        self.hessian_cache = {}
