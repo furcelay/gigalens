c49a80d100376d4526f9a97c2f88f8126e935aca
furcelay <fjurcelay@uc.cl>
2024-01-18 13:09:12 -0300

    update mass profiles __init__.py

--- src/gigalens/tf/profiles/mass/__init__.py
+++ src/gigalens/tf/profiles/mass/__init__.py
@@ -5,3 +5,9 @@ import gigalens.tf.profiles.mass.sis
 import gigalens.tf.profiles.mass.tnfw
 import gigalens.tf.profiles.mass.nfw
 import gigalens.tf.profiles.mass.piemd
+import gigalens.tf.profiles.mass.piep
+import gigalens.tf.profiles.mass.scaling_relation
+import gigalens.tf.profiles.mass.scaling_series
+import gigalens.tf.profiles.mass.dpie_subhalo
+import gigalens.tf.profiles.mass.dpie_subhalo_series
+
