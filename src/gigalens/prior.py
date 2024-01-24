0f52c8efacb99b9a0c23e20d27bf45cd2fe1890c
furcelay <fjurcelay@uc.cl>
2024-01-24 15:46:01 -0300

    merge_dict moved to util module

--- src/gigalens/prior.py
+++ src/gigalens/prior.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+from gigalens.util import merge_dicts
 
 
 class ProfilePriorBase:
@@ -133,22 +134,3 @@ class LensPriorBase:
 
     def __repr__(self):
         return f"lenses: {self.lenses} | sources: {self.sources} | foreground: {self.foreground}"
-
-
-def _merge_dicts(d1, d2):  # TODO: move this to a utils module
-    """
-    Merge two nested dictionaries into a new one without modifying the originals.
-    Raises ValueError in case of conflicts.
-    """
-    merged = {}
-    for key in d1.keys() | d2.keys():  # Union of keys from both dictionaries
-        if key in d1 and key in d2:  # Key is in both dictionaries
-            if isinstance(d1[key], dict) and isinstance(d2[key], dict):  # Both values are dictionaries
-                merged[key] = _merge_dicts(d1[key], d2[key])  # Recursively merge them
-            else:
-                raise ValueError(f"Conflict: {key} parameter is in both dictionaries, cannot safely merge them")
-        elif key in d1:  # Key is only in d1
-            merged[key] = d1[key]
-        else:  # Key is only in d2
-            merged[key] = d2[key]
-    return merged
