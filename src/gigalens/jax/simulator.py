d80b6aa036c4abccd5a2a649b179dd29ca576e79
felipe <fjurcelay@uc.cl>
2024-03-29 22:32:22 -0300

    minor syntax correction

--- src/gigalens/jax/simulator.py
+++ src/gigalens/jax/simulator.py
@@ -94,7 +94,7 @@ class LensSimulator(gigalens.simulator.LensSimulatorInterface):
                                params):
         beta_points = []
         beta_barycentre = []
-        for x_i, y_i, sp in zip(x, y):
+        for x_i, y_i in zip(x, y):
             beta_points_i = jnp.stack(self.beta_1(x_i, y_i, params['lens_mass']), axis=0)
             beta_points_i = jnp.transpose(beta_points_i, (2, 0, 1))  # batch size, xy, images
             beta_barycentre_i = jnp.mean(beta_points_i, axis=2, keepdims=True)
@@ -111,7 +111,7 @@ class LensSimulator(gigalens.simulator.LensSimulatorInterface):
         deflection_ratio = params.get('deflection_ratio', self.phys_model.deflection_ratio_constants)
         beta_points = []
         beta_barycentre = []
-        for x_i, y_i, sp in zip(x, y):
+        for x_i, y_i in zip(x, y):
             beta_points_i = jnp.stack(self.beta_2(x_i, y_i,
                                                   params['lens_mass'], source_mass_1,
                                                   deflection_ratio), axis=0)
@@ -157,7 +157,7 @@ class LensSimulator(gigalens.simulator.LensSimulatorInterface):
                                params):
         deflection_ratio = params.get('deflection_ratio', self.phys_model.deflection_ratio_constants)
         magnifications = []
-        for x_i, y_i, sp in zip(x, y):
+        for x_i, y_i in zip(x, y):
             magnifications.append(self.magnification_1(x_i, y_i, params['lens_mass'], deflection_ratio))
         return magnifications
 
@@ -169,7 +169,7 @@ class LensSimulator(gigalens.simulator.LensSimulatorInterface):
         source_mass_1 = self.phys_model.shared_mass_light(params['source_mass_1'], params['source_light_1'])
         deflection_ratio = params.get('deflection_ratio', self.phys_model.deflection_ratio_constants)
         magnifications = []
-        for x_i, y_i, sp in zip(x, y):
+        for x_i, y_i in zip(x, y):
             magnifications.append(self.magnification_2(x_i, y_i,
                                                        params['lens_mass'], source_mass_1,
                                                        deflection_ratio))
