cd3e78945f06be1a0a18741920fb5a8810a99d8a
furcelay <fjurcelay@uc.cl>
2024-04-11 17:04:58 -0400

    added point source profile to be able to include constraints with positions without pixels for some sources

---
+++ src/gigalens/jax/profiles/light/point_source.py
@@ -0,0 +1,20 @@
+import functools
+
+import jax.numpy as jnp
+from jax import jit
+
+import gigalens.profile
+
+
+class PointSource(gigalens.profile.LightProfile):
+    _name = "POINT_SOURCE"
+    _params = []
+    _amp = "amp"
+
+    def __init__(self, use_lstsq=False, is_source=True):
+        super(PointSource, self).__init__(use_lstsq=use_lstsq, is_source=is_source)
+
+    @functools.partial(jit, static_argnums=(0,))
+    def light(self, x, y):
+        ret = jnp.zeros_like(x)
+        return ret[jnp.newaxis, ...] if self.use_lstsq else ret
---
+++ src/gigalens/tf/profiles/light/point_source.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+import gigalens.profile
+
+
+class PointSource(gigalens.profile.LightProfile):
+    _name = "POINT_SOURCE"
+    _params = []
+    _amp = "amp"
+
+    def __init__(self, use_lstsq=False, is_source=True):
+        super(PointSource, self).__init__(use_lstsq=use_lstsq, is_source=is_source)
+
+    @tf.function
+    def light(self, x, y):
+        ret = tf.zeros_like(x)
+        return ret[tf.newaxis, ...] if self.use_lstsq else ret
